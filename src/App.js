04185f1901858760a71b727b162c505808e88e89
yash <jewalkarayash@gmail.com>
2024-03-11 10:24:32 +0530

    added edit profile page

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Routes, Route } from 'react-router-dom';
+import Login from './Components/Login';
+import Home from './Components/Home';
+import {BrowserRouter} from "react-router-dom";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Privacy from "./Components/Privacy";
+import home_back from './assets/home_back.png'
+import SignUp from "./Components/SignUp";
+const App = () => {
+    return (
+      <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<Login />} />
+                <Route path={"/about"} element={<About/>} />
+                <Route path={"/contact"} element={<Contact/>} />
+                <Route path={"/privacy"} element={<Privacy/>} />
+                <Route path={"/sign-up"} element={<SignUp/>} />
+                <Route
+                    path={"/home/*"}
+                    element={
+                    <ProtectedRoute>
+                        <Home/>
+                    </ProtectedRoute>
+                    }
+                />
+            </Routes>
+      </BrowserRouter>
+  );
+};
+
+export default App;
